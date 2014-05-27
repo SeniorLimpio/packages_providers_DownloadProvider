295005c2b18711a6f0dcb0a3d0b6af9c53f4dbc2
SeniorLimpio <seniorlimpio1@gmail.com>
2014-05-28 08:48:18 +1200

    Revert "DownloadProvider: Add transfer speeds in notification"
    This reverts commit 759696d18c10e68f50f2d30d27822f28efd611d7.

--- src/com/android/providers/downloads/DownloadNotifier.java
+++ src/com/android/providers/downloads/DownloadNotifier.java
@@ -43,7 +43,6 @@ import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Multimap;
 
-import java.text.DecimalFormat;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -86,11 +85,6 @@ public class DownloadNotifier {
      */
     @GuardedBy("mDownloadSpeed")
     private final LongSparseLongArray mDownloadTouch = new LongSparseLongArray();
-
-    /**
-     * Formatter for giving transfer speeds with maximum of one decimal places
-     */
-    private final DecimalFormat mFormatter = new DecimalFormat("#.#");
 
     public DownloadNotifier(Context context) {
         mContext = context;
@@ -209,7 +203,6 @@ public class DownloadNotifier {
             // Calculate and show progress
             String remainingText = null;
             String percentText = null;
-            String speedText = null;
             if (type == TYPE_ACTIVE) {
                 long current = 0;
                 long total = 0;
@@ -229,23 +222,6 @@ public class DownloadNotifier {
                     percentText = res.getString(R.string.download_percent, percent);
 
                     if (speed > 0) {
-                        // Determine postfix for download speed (B/s, KB/s or MB/s)
-                        String postFix = null;
-                        double speedNormalized = 0.0;
-
-                        if (speed < 1024) {
-                            postFix = " B/s";
-                            speedNormalized = speed;
-                        } else if (speed < 1048576) {
-                            postFix = " KB/s";
-                            speedNormalized = (double)speed / 1024.0;
-                        } else if (speed < 1073741824) {
-                            postFix = " MB/s";
-                            speedNormalized = (double)speed / 1048576.0;
-                        }
-
-                        speedText = mFormatter.format(speedNormalized) + postFix;
-
                         final long remainingMillis = ((total - current) * 1000) / speed;
                         remainingText = res.getString(R.string.download_remaining,
                                 DateUtils.formatDuration(remainingMillis));
@@ -260,16 +236,9 @@ public class DownloadNotifier {
             // Build titles and description
             final Notification notif;
             if (cluster.size() == 1) {
-                final Notification.InboxStyle inboxStyle = new Notification.InboxStyle(builder);
-
                 final DownloadInfo info = cluster.iterator().next();
 
-                final String filename = getDownloadTitle(res, info).toString();
-
-                inboxStyle.addLine(filename);
-                builder.setContentTitle(filename);
-
-                String contentText = null;
+                builder.setContentTitle(getDownloadTitle(res, info));
 
                 if (type == TYPE_ACTIVE) {
                     if (!TextUtils.isEmpty(info.mDescription)) {
@@ -283,14 +252,6 @@ public class DownloadNotifier {
                     } else {
                         inboxStyle.setSummaryText(speedText + ", " + remainingText);
                     }
-
-                    if (TextUtils.isEmpty(speedText) || TextUtils.isEmpty(remainingText)) {
-                        contentText = res.getString(R.string.download_running);
-                    } else {
-                        contentText = speedText + ", " + remainingText;
-                    }
-
-                    inboxStyle.setSummaryText(contentText);
                     builder.setContentInfo(percentText);
 
                 } else if (type == TYPE_WAITING) {
@@ -311,7 +272,7 @@ public class DownloadNotifier {
                     }
                 }
 
-                notif = inboxStyle.build();
+                notif = builder.build();
 
             } else {
                 final Notification.InboxStyle inboxStyle = new Notification.InboxStyle(builder);
